af9260a771562c17891d2301725799fe5f1834ad
Josh Scoggins <jscoggins@parasoft.com>
2020-01-27 09:52:22 -0800

    Cleanup test program further

--- TestProgram.cc
+++ TestProgram.cc
@@ -1,22 +1,31 @@
 #include "BinaryManipulation.h"
 #include <iostream>
 
+template<typename T>
+void outputToCout(T value) noexcept {
+    using K = std::decay_t<T>;
+    std::cout << "0x" << std::hex;
+    // we want to cast the char to an int to make sure that garbage is not printed out
+    if constexpr (std::is_same_v<K, signed char> || std::is_same_v<K, unsigned char>) {
+        std::cout << static_cast<int>(value);
+    } else {
+        std::cout << value;
+    }
+    std::cout << std::endl;
+}
 void test0() {
     std::cout << "Simple test 0 of single decoder" << std::endl;
     auto contents = BinaryManipulation::unpack<uint32_t, BinaryManipulation::Ordinal32AsLittleEndianBytes>(0xFDEDABCD);
     // we get a tuple of tuples back
     auto [l0, l1, h0, h1] = contents;
-    auto fn = [](auto value) noexcept {
-        std::cout << "0x" << std::hex << static_cast<int>(value) << std::endl;
-    };
-    fn(l0);
-    fn(l1);
-    fn(h0);
-    fn(h1);
+    outputToCout(l0);
+    outputToCout(l1);
+    outputToCout(h0);
+    outputToCout(h1);
 
     // put the tuple back in
     auto combination = BinaryManipulation::pack<uint32_t, BinaryManipulation::Ordinal32AsLittleEndianBytes>(std::move(contents));
-    fn(combination);
+    outputToCout(combination);
 }
 void test1() {
     std::cout << "Simple test 1: Single NestedEncoderDecoer" << std::endl;
@@ -27,18 +36,15 @@ void test1() {
     auto [l0, l1, h0, h1] = quarters;
     auto [lower, upper] = halves;
 
-    auto fn = [](auto value) noexcept {
-        std::cout << "0x" << std::hex << static_cast<int>(value) << std::endl;
-    };
-    fn(l0);
-    fn(l1);
-    fn(h0);
-    fn(h1);
-    fn(lower);
-    fn(upper);
+    outputToCout(l0);
+    outputToCout(l1);
+    outputToCout(h0);
+    outputToCout(h1);
+    outputToCout(lower);
+    outputToCout(upper);
 
     auto combination = BinaryManipulation::pack<uint32_t, TestEncoderDecoder>(std::move(result));
-    fn(combination);
+    outputToCout(combination);
 }
 int main() {
     test0();
